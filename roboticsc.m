35519a7f18ee15eb0966a814248b6a81663eb10d
Brad Saund <brad.saund@gmail.com>
2015-09-28 01:51:36 -0400

    more cleanup and renaming

--- roboticsc.m
+++ roboticsc.m
@@ -1,5 +1,5 @@
 function roboticsc(varargin)
-	global tts ss5 T2 f kkx kky kkz
+	global endEffectorDisplay ss5 T2 f kkx kky kkz
 	figure('un','n','nam','Robot','color','w','pos',[.1 .1 .8 .8])
 	
 	ma = axes('pos',[.15 .15 1 1]);
@@ -111,7 +111,7 @@ function roboticsc(varargin)
 		axisType=get(axisMotionTypeDisplay,'string');
 		delete(findobj('type','uicontrol','style','slider'));
 		delete(findobj('type','uicontrol','style','text'));
-		tts=uicontrol('sty','te','backgroundcolor','w','un','n','pos',[.7 0 .2 .05],'string','','fontsize',15);
+        endEffectorDisplay=uicontrol('sty','te','backgroundcolor','w','un','n','pos',[.7 0 .2 .05],'string','','fontsize',15);
 		
         %Set up axis postion sliders
 		for k=1:length(a)
@@ -168,53 +168,55 @@ function roboticsc(varargin)
 		axis on
 		set(dhParameterDisplay,'value',1)
 		
-		ll=flipud(findobj('type','uicontrol','style','slider'));
+		sliders=flipud(findobj('type','uicontrol','style','slider'));
 		cla;
 		hold on
 		plot3(0,0,0,'r*')
 		
-		DH=(str2num(get(dhParameterDisplay,'string'))); %#ok<*ST2NM>
+		DH=str2num(get(dhParameterDisplay,'string')); %#ok<*ST2NM>
 		
 		al=DH(:,1);
 		a=DH(:,2);
 		d=DH(:,3);
 		t=DH(:,4);
+        
+        numLinks = length(a);
 		
-		w=get(axisMotionTypeDisplay,'string');
-		n=length(a);
-		
-		for k=1:length(a)
-			if strcmp(w(k),'t')
-				t(k)=get(ll(k),'value');
-			else
-				d(k)=get(ll(k),'value');
+		motionType=get(axisMotionTypeDisplay,'string');
+	
+		%Get values from sliders
+		for k=1:numLinks
+			if strcmp(motionType(k),'t') %Angular link
+				t(k)=get(sliders(k),'value');
+            else  %Linear Link
+				d(k)=get(sliders(k),'value');
 			end
 		end
 		
-		FFF=num2str([al,a,d,t],5);
-		set(dhParameterDisplay,'string',FFF)
+		allDhParams=num2str([al,a,d,t],5);
+		set(dhParameterDisplay,'string',allDhParams)
 		T=cell(length(a),1);
 		f=1;
-		for k=1:length(a)
+		for k=1:numLinks
 			T{k}=trans(al(k),a(k),d(k),t(k));
 		end
 		
-		T2=cell(n,1);
+		T2=cell(numLinks,1);
 		T2{1}=T{1};
-		for k=2:n
+		for k=2:numLinks
 			T2{k}=T2{k-1}*T{k};
 		end
 		
 		pv=plotorg([eye(4),[0;0;0;0]]);
-		for k=1:n
-			if w(k)=='d'
-				T2{k}=T2{k}+[0,0,0,0;0,0,0,0;0,0,0,0;1,0,0,0];
-			else
-				T2{k}=T2{k}.*(1-[0,0,0,0;0,0,0,0;0,0,0,0;1,0,0,0]);
-			end
-			if k<n && abs(a(k+1))>1e-10 && abs(d(k))>1e-10
+		for k=1:numLinks
+% 			if motionType(k)=='d'
+% 				T2{k}=T2{k}+[0,0,0,0;0,0,0,0;0,0,0,0;1,0,0,0];
+% 			else
+% 				T2{k}=T2{k}.*(1-[0,0,0,0;0,0,0,0;0,0,0,0;1,0,0,0]);
+% 			end
+			if k<numLinks && abs(a(k+1))>1e-10 && abs(d(k))>1e-10
 				pv=plotorg([double(T2{k}),[pv;k]],'t',[a(k+1),d(k),al(k)]);
-			elseif k<n && abs(a(k+1))>1e-10
+			elseif k<numLinks && abs(a(k+1))>1e-10
 				pv=plotorg([double(T2{k}),[pv;k]],'a',a(k+1));
 			elseif abs(d(k))>1e-10
 				pv=plotorg([double(T2{k}),[pv;k]],'d',[d(k),al(k)]);
@@ -224,17 +226,16 @@ function roboticsc(varargin)
 		end
 		
 		% 		axis([-ss5-2 ss5+2 -ss5-2 ss5+2 -ss5-2 ss5+2])
-		PPPPP=T2{end};
-		set(tts,'string',sprintf('%2.2f ',transpose(PPPPP(1:3,4))));
+		cartesianEndEffector=T2{end};
+		set(endEffectorDisplay,'string',sprintf('%2.2f ',cartesianEndEffector(1:3,4)'));
 		
-		dddd=T2{end};
-		disp((dddd))
+		disp(cartesianEndEffector)
 		
 		if strcmp(get(dumd,'string'),'0')
-			JL = zeros(length(w),2);
-			for klk = 1:length(w)
+			JL = zeros(length(motionType),2);
+			for klk = 1:length(motionType)
 				
-				if strcmp(w(klk),'t')
+				if strcmp(motionType(klk),'t')
 					JL(klk,:) = [0,2*pi];
 				else
 					JL(klk,:) = [-ss5,ss5];
@@ -242,7 +243,7 @@ function roboticsc(varargin)
 				
 			end
 			
-			[kkx,kky,kkz] = mc(DH,w,JL);
+			[kkx,kky,kkz] = mc(DH,motionType,JL);
 			set(dumd,'string','1');
 		end
 		axis([min(kkx),max(kkx),min(kky),max(kkx),min(kkz),max(kkz)]);
@@ -250,7 +251,7 @@ function roboticsc(varargin)
 		function [pv,g]=plotorg(T,varargin)
 			
 			nuk = T(end);
-			w=get(axisMotionTypeDisplay,'string');
+			motionType=get(axisMotionTypeDisplay,'string');
 			
 			res=10;
 			R=T(1:3,1:3);
@@ -304,12 +305,12 @@ function roboticsc(varargin)
 					rotate(mor,[0,1,0],90,P)
 					
 				elseif strcmp(varargin{1},'d')
-					ll=varargin{2};
+					sliders=varargin{2};
 					[A,B,C]=cylinder(.5,res);
-					mor=surf(A+P(1),B+P(2),C.*ll(1)+P(3));
+					mor=surf(A+P(1),B+P(2),C.*sliders(1)+P(3));
 					[A,B,C]=sphere(res);
-					ppj1=surf(A.*.5+P(1),B.*.5+P(2),C.*.5+P(3)-ll(1));
-					if strcmp(w(T(4,5)),'d')
+					ppj1=surf(A.*.5+P(1),B.*.5+P(2),C.*.5+P(3)-sliders(1));
+					if strcmp(motionType(T(4,5)),'d')
 						set(mor,'facecolor',[.14 .18 .14])
 						set(ppj1,'facecolor',[.4 .8 .4],'facealpha',.5,'facecolor','y')
 					else
@@ -319,18 +320,18 @@ function roboticsc(varargin)
 					rotate(mor,[0,1,0],180,P)
 					ppj=[ppj,ppj1];
 				else
-					ll=varargin{2};
+					sliders=varargin{2};
 					[A,B,C]=cylinder(.5,res);
-					mor1=surf(A+P(1),B+P(2),C.*ll(1)+P(3));
+					mor1=surf(A+P(1),B+P(2),C.*sliders(1)+P(3));
 					set(mor1,'facecolor',[.4 .8 .4])
 					rotate(mor1,[0,1,0],90,P)
 					
 					[A,B,C]=cylinder(.5,res);
-					mor=surf(A+P(1),B+P(2),C.*ll(2)+P(3));
+					mor=surf(A+P(1),B+P(2),C.*sliders(2)+P(3));
 					[A,B,C]=sphere(res);
-					ppj2=surf(A.*.5+P(1),B.*.5+P(2),C.*.5+P(3)-ll(2));
+					ppj2=surf(A.*.5+P(1),B.*.5+P(2),C.*.5+P(3)-sliders(2));
 					
-					if strcmp(w(T(4,5)),'d')
+					if strcmp(motionType(T(4,5)),'d')
 						set(mor,'facecolor',[.14 .18 .14])
 						set(ppj2,'edgecolor','none','facecolor','y')
 					else
@@ -353,7 +354,7 @@ function roboticsc(varargin)
 			
 			T12=[t1 t2 t3];
 			
-			if nuk == length(w)
+			if nuk == length(motionType)
 				
 				x=P(1);
 				y=P(2);
